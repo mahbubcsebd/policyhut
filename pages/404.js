aa3e1c5de18472e811629025b975f872a46e893d
Mahbubur Rahman <77193562+mahbubcsebd@users.noreply.github.com>
2023-11-06 15:48:24 +0600

    update

--- pages/404.js
+++ pages/404.js
@@ -1,15 +1,19 @@
 import "@/app/globals.css";
-import Header from '@/components/global/Header';
 import error from '@/public/images/404.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { Col, Container, Row } from 'react-bootstrap';
+import styles from './404.module.css';
 
 function PageNotFound() {
+    const router = useRouter();
+    setTimeout(() => {
+        router.push('/');
+    }, 5000);
     return (
         <div className="">
-            <Header />
             <div
                 id="notFoundPage"
                 className="notFoundPage"
@@ -17,24 +21,24 @@ function PageNotFound() {
                 <div className="notFoundArea section">
                     <Container>
                         <Row className="justify-content-center">
-                            <Col lg={8}>
-                                <div className="notFound-content">
+                            <Col lg={6}>
+                                <div className={styles.notFoundContent}>
                                     <Image
                                         src={error}
                                         alt="404"
                                     />
 
-                                    <h2 className="notFoundTitle">
+                                    <h2 className={styles.notFoundTitle}>
                                         404 Page Not Found
                                     </h2>
-                                    <p className="notFoundText">
+                                    <p className={styles.notFoundText}>
                                         This page is incidentally inaccessible
                                         because of support. We will back very
                                         before long much obliged for your
                                         understanding
                                     </p>
                                     <Link
-                                        className="btn btn-orange"
+                                        className="btnBlue"
                                         href="/"
                                     >
                                         Go Back To Homepage
